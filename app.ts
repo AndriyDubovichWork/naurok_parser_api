248d3c95f910346c192f6ed8b0430a99b522b253
AndriyDubovichWork <86286088+AndriyDubovichWork@users.noreply.github.com>
2022-11-13 11:15:27 +0300

    setup app on new pc

--- app.ts
+++ app.ts
@@ -7,11 +7,11 @@ const hostName = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-  res.send(await getAnswers());
+	res.send(await getAnswers());
 });
 
 app.listen(port, hostName, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
 
 ('https://naurok.com.ua/site/search-resources?q=%27%D0%90%D0%B4%D0%B0%D0%BF%D1%82%D0%B0%D1%86%D1%96%D1%8F%27&type%5B0%5D=test&grade%5B0%5D=11&subject%5B0%5D=3&page=16');
--- src/getAnswers.ts
+++ src/getAnswers.ts
@@ -4,28 +4,25 @@ import getMatchedByQuestion from './getMatchedByQuestion';
 import getTestsWithSameQuestionsNumber from './getTestsWithSameQuestionsNumber';
 
 const getAnswers = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
 
-  const AllPagesData = await getAllPages(
-    page,
-    'україна в умовах десталінізації',
-    '11',
-    '8'
-  );
-  if (AllPagesData === 'error') {
-    return 'error';
-  }
-  const TestsWithCorrectQuestionsQuantity =
-    await getTestsWithSameQuestionsNumber(AllPagesData, '23');
+	const AllPagesData = await getAllPages(page, 'україна в умовах десталінізації', '11', '8');
+	if (AllPagesData === 'error') {
+		return 'error';
+	}
+	const TestsWithCorrectQuestionsQuantity = await getTestsWithSameQuestionsNumber(
+		AllPagesData,
+		'23'
+	);
 
-  // const matched = await getMatchedByQuestion(
-  //   page,
-  //   TestsWithCorrectQuestionsQuantity,
-  //   'Здатність організмів'
-  // );
-  // console.log(matched);
-  browser.close();
-  return TestsWithCorrectQuestionsQuantity;
+	// const matched = await getMatchedByQuestion(
+	//   page,
+	//   TestsWithCorrectQuestionsQuantity,
+	//   'Здатність організмів'
+	// );
+	// console.log(matched);
+	browser.close();
+	return TestsWithCorrectQuestionsQuantity;
 };
 export default getAnswers;
