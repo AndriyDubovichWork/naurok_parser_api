19d37b3c898b5234e853289f4417789a24d506fa
AndriyDubovichWork <86286088+AndriyDubovichWork@users.noreply.github.com>
2022-11-18 15:58:16 +0200

    0.0.0.0

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const link = '0.0.0.0';
 
 app.use(express.json());
 
@@ -16,7 +17,7 @@ app.get('/', async (req, res) => {
 	}
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.listen(port, link, () => {
+	console.log(`Example app listening on port ${link}:${port}`);
 });
 ('http://localhost:3000/?topic=%D1%83%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0%20%D0%B2%20%D1%83%D0%BC%D0%BE%D0%B2%D0%B0%D1%85%20%D0%B4%D0%B5%D1%81%D1%82%D0%B0%D0%BB%D1%96%D0%BD%D1%96%D0%B7%D0%B0%D1%86%D1%96%D1%97&grade=11&subjectID=8&questionsQuantity=23');
