107f84fe8ee8ae7ac1f4cef535098e5c0ad9c228
AndriyDubovichWork <86286088+AndriyDubovichWork@users.noreply.github.com>
2022-12-25 20:58:22 +0200

    puppeteer-chromium-resolver

--- src/getAnswers.ts
+++ src/getAnswers.ts
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const getAllPages = require('./getAllPages');
 // const getMatchedByQuestion = require('./getMatchedByQuestion');
 const getTestsWithSamequestionsQuantity = require('./getTestsWithSamequestionsQuantity');
+const PCR = require('puppeteer-chromium-resolver');
 
 const getAnswers = async (
 	topic: string,
@@ -9,10 +10,23 @@ const getAnswers = async (
 	subjectID: string,
 	questionsQuantity: string
 ) => {
+	const option = {
+		revision: '',
+		detectionPath: '',
+		folderName: '.chromium-browser-snapshots',
+		defaultHosts: ['https://storage.googleapis.com', 'https://npm.taobao.org/mirrors'],
+		hosts: [],
+		cacheRevisions: 2,
+		retry: 3,
+		silent: false,
+	};
+	const stats = await PCR(option);
+
 	const browser = await puppeteer.launch({
 		headless: true,
 		defaultViewport: null,
 		args: ['--no-sandbox', '--headless'],
+		executablePath: stats.executablePath,
 	});
 	const page = await browser.newPage();
 	await page.setDefaultNavigationTimeout(0);
